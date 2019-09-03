d13648e70d688daf53f7094fa6ca9929f305d71b
gness1804 <graham.nessler@gmail.com>
2019-09-03 13:35:28 -0500

    Add Item model

---
+++ src/models/Item.js
@@ -0,0 +1,10 @@
+class Item {
+  constructor(opts) {
+    this.name = opts.name;
+    this.aisle = opts.aisle;
+    this.quantity = opts.quantity;
+    this.note = opts.note;
+  }
+}
+
+export default Item;
