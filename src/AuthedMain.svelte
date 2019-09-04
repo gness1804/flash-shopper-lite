5f5894f70380ed9eced818abd4006cfdf8c138e0
gness1804 <graham.nessler@gmail.com>
2019-09-04 13:00:09 -0500

    Break out sorting functions into helper methods

--- src/AuthedMain.svelte
+++ src/AuthedMain.svelte
@@ -67,6 +67,54 @@
     }
   }
 
+  // sort helper methods
+  /** Sorts array alphabetically by 'name' property.
+  * @param {Object[]} arr - the array to sort.
+  * @returns {Object[]} - sorted array.
+  */
+  const sortAlpha = (arr) => arr.sort((a, b) => {
+        const lowerA = a.name.toLowerCase();
+        const lowerB = b.name.toLowerCase();
+        if (lowerA < lowerB) {
+          return -1;
+        }
+        if (lowerA > lowerB) {
+          return 1;
+        }
+        return 0;
+  });
+
+  /** Sorts array numerically by 'aisle' property.
+   * If aisle is undefined, assign 0 value and put item to top of the array.
+  * @param {Object[]} arr - the array to sort.
+  * @returns {Object[]} - sorted array.
+  */
+  const sortAisle = (arr) => arr.sort((a, b) => {
+        const parsedA = parseFloat(a.aisle) || 0;
+        const parsedB = parseFloat(b.aisle) || 0;
+        if (parsedA < parsedB) {
+          return -1;
+        }
+        if (parsedA > parsedB) {
+          return 1;
+        }
+        return 0;
+  });
+
+   /** Sorts array numerically by 'id' property, which is Date.now() when each item was created.
+  * @param {Object[]} arr - the array to sort.
+  * @returns {Object[]} - sorted array.
+  */
+  const sortDate = (arr) => arr.sort((a, b) => {
+        if (a.id < b.id) {
+          return -1;
+        }
+        if (a.id > b.id) {
+          return 1;
+        }
+        return 0;
+  });
+
   onMount(() => {
 		updateItemsFromCookie();
 	});
@@ -110,43 +158,15 @@
   </select>
   <div class="authed-main-items-container">
     {#if sortState === 'alpha'}
-      {#each $items.sort((a, b) => {
-        const lowerA = a.name.toLowerCase();
-        const lowerB = b.name.toLowerCase();
-        if (lowerA < lowerB) {
-          return -1;
-        }
-        if (lowerA > lowerB) {
-          return 1;
-        }
-        return 0;
-  }) as item (item.id)}
+      {#each sortAlpha($items) as item (item.id)}
         <ItemContainer {...item} on:deleteItem={deleteItem} on:updateItem={updateItem} />
       {/each}
     {:else if sortState === 'aisle'}
-      {#each $items.sort((a, b) => {
-        const parsedA = parseFloat(a.aisle) || 0;
-        const parsedB = parseFloat(b.aisle) || 0;
-        if (parsedA < parsedB) {
-          return -1;
-        }
-        if (parsedA > parsedB) {
-          return 1;
-        }
-        return 0;
-  }) as item (item.id)}
+      {#each sortAisle($items) as item (item.id)}
         <ItemContainer {...item} on:deleteItem={deleteItem} on:updateItem={updateItem} />
       {/each}
     {:else if sortState === 'date'}
-      {#each $items.sort((a, b) => {
-        if (a.id < b.id) {
-          return -1;
-        }
-        if (a.id > b.id) {
-          return 1;
-        }
-        return 0;
-  }) as item (item.id)}
+      {#each sortDate($items) as item (item.id)}
         <ItemContainer {...item} on:deleteItem={deleteItem} on:updateItem={updateItem} />
       {/each}
     {/if}
