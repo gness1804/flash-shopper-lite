eec1762ac44b9f1d580e6e2ea5ba70bda571bf8e
gness1804 <graham.nessler@gmail.com>
2019-09-04 11:30:40 -0500

    Removed derived stores from store

--- src/stores/mainStore.js
+++ src/stores/mainStore.js
@@ -1,21 +1,3 @@
-import { writable, derived } from 'svelte/store';
+import { writable } from 'svelte/store';
 
 export const items = writable([]);
-
-export const itemsSortedByAlpha = derived(
-  items,
-  $items => $items.sort((a, b) => {
-    if (!a.name || !b.name) {
-      return 0;
-    }
-    const lowerA = a.name.toLowerCase();
-    const lowerB = b.name.toLowerCase();
-    if (lowerA < lowerB) {
-      return -1;
-    }
-    if (lowerA > lowerB) {
-      return 1;
-    }
-    return 0;
-  })
-);
