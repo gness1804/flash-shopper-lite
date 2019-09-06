10f3c79bed80afb323821d40740eee9838338e29
gness1804 <graham.nessler@gmail.com>
2019-09-06 11:26:03 -0500

    Refactor to reduce dom nodes and and some more CSS

--- src/GroceryAPIResults.svelte
+++ src/GroceryAPIResults.svelte
@@ -9,8 +9,13 @@
       margin-bottom: 40px;
     }
 
-    .grocery-api-results-each-result-title {
+    .grocery-api-results-each-result-title-link {
       margin-bottom: 25px;
+      display: block;
+    }
+
+    .grocery-api-results-each-result-title-link:nth-child(odd) {
+      background-color: #d8d9da;
     }
   </style>
 
@@ -18,11 +23,14 @@
  {#if parsedData && parsedData.data && parsedData.data.children}
   <h2 class="grocery-api-results-header">What are grocery shoppers on Reddit interested in?</h2>
   {#each parsedData.data.children as { data: _data } (_data.id)}
-    <div class="grocery-api-results-each-result-container">
-      <a href={`https://reddit.com${_data.permalink}`} target="_blank">
-        <p class="grocery-api-results-each-result-title">{_data.title}</p>
+      <a
+        href={`https://reddit.com${_data.permalink}`}
+        target="_blank"
+        class="grocery-api-results-each-result-title-link"
+        title={_data.title}
+      >
+        {_data.title}
       </a>
-    </div>
   {/each}
  {:else }
   <p>No data found...</p>
