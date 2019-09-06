9ce009c7efd3482c442ce725c558bef9bbfb487d
gness1804 <graham.nessler@gmail.com>
2019-09-06 10:42:42 -0500

    Add link to title for reddit results

--- src/GroceryAPIResults.svelte
+++ src/GroceryAPIResults.svelte
@@ -12,7 +12,9 @@
   <h3>What are grocery shoppers on Reddit interested in?</h3>
   {#each parsedData.data.children as { data: _data } (_data.id)}
     <div class="grocery-api-results-each-result-container">
-      <p>{_data.title}</p>
+      <a href={`https://reddit.com${_data.permalink}`} target="_blank">
+        <p>{_data.title}</p>
+      </a>
     </div>
   {/each}
  {:else }
