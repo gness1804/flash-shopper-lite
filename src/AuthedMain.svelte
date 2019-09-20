7aba35f6805c4c591e9b748e763a9fb47dccfcd9
gness1804 <graham.nessler@gmail.com>
2019-09-20 11:44:52 -0500

    Convert AuthedMain to Tailwind

--- src/AuthedMain.svelte
+++ src/AuthedMain.svelte
@@ -122,43 +122,13 @@
 </script>
 
 <style>
-  .authed-main-top-message-container {
-    margin-bottom: 40px;
-  }
-
-  .authed-main-delete-all-items-button {
+  .delete-all-items-button {
     background-color: #f00;
-    color: #fff;
-    margin-bottom: 0;
-    margin-right: 10px;
-  }
-
-  .authed-main-my-store-selector-container {
-    margin-bottom: 40px;
-  }
-
-  .authed-main-controls-top-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 0.5rem;
-  }
-
-  .authed-main-sort-type-selector {
-     margin-bottom: 0;
-  }
-
-  .authed-main-sort-type-label {
-    margin-right: 10px;
-  }
-
-  .authed-main-my-store-selector-label:hover {
-    cursor: pointer;
   }
 </style>
 
 <div class="authed-main">
-  <div class="authed-main-top-message-container">
+  <div class="mb-10">
     {#if user}
     <p>Hello, {user.name}</p>
     {/if}
@@ -172,21 +142,21 @@
   <ItemInput on:addItem={addItem} on:showToast>
     <h2 slot="title">Add an Item!</h2>
   </ItemInput>
-  <div class="authed-main-controls-top-container">
-    <button on:click={deleteAllItems} disabled={itemsCount === 0} class="authed-main-delete-all-items-button">
+  <div class="flex justify-center items-center mb-2">
+    <button on:click={deleteAllItems} disabled={itemsCount === 0} class="delete-all-items-button text-white mb-0 mr-2">
       Delete All Items
     </button>
-    <span class="authed-main-sort-type-label">Sort By:</span>
-    <select bind:value={sortState} class="authed-main-sort-type-selector">
+    <span class="mr-2">Sort By:</span>
+    <select bind:value={sortState} class="mb-0">
       {#each sortOptions as { name, value }}
         <option value={value}>{name}</option>
       {/each}
     </select>
   </div>
-  <div class="authed-main-my-store-selector-container">
+  <div class="mb-10">
     <h4>My Store:</h4>
     {#each groceryStores as { name, value }}
-      <label class="authed-main-my-store-selector-label">
+      <label class="cursor-pointer">
         <input type="radio" value={value} bind:group={selectedGroceryStore} />
         {name}
       </label>
