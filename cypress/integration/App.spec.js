6df23c3d9b96af51e2830c7c3b7cd79ba0ed76e1
gness1804 <graham.nessler@gmail.com>
2019-09-23 16:44:38 -0500

    Setup eslint for cypress

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -2,8 +2,8 @@ describe('App Landing Page', function () {
   it('Contains the correct footer text', function () {
     cy.visit('/');
     cy
-    .get('#app')
-    .find('#app-header')
-    .then(elem => expect(elem).to.contain('Flash Shopper Lite'))
-  })
-})
+      .get('#app')
+      .find('#app-header')
+      .then(elem => expect(elem).to.contain('Flash Shopper Lite'));
+  });
+});
