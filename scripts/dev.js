52f62453d5210d285105005712ac25508afa6550
gness1804 <graham.nessler@gmail.com>
2019-10-09 13:14:38 -0500

    Add console info message telling user that app is being served up in dev mode

--- scripts/dev.js
+++ scripts/dev.js
@@ -3,6 +3,9 @@ const handleOutput = require('./handleOutput');
 
 const arg = process.argv[2];
 
+// eslint-disable-next-line no-console
+console.info('Serving up the app on http://localhost:8080');
+
 if (arg === '--open') {
   execSync(
     "open 'http://localhost:8080' && webpack-dev-server --content-base public",
